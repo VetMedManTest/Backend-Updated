dd27a80a2889d6797f746167ea82067e888d4ad2
saranshsinhaa <saranshsinha084@gmail.com>
2023-06-23 21:34:00 +0530

    :building_construction: Add rfq API controller

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const order = require("./routes/orderRoutes");
 const varieties = require("./routes/varietyRoutes");
 const contactUs = require("./routes/contactUsRoutes");
 const healthCheck = require("./routes/healthCheckRoutes");
-//
+const rfq = require("./routes/rfqRoutes");
 
 app.use("/api/v1", user);
 app.use("/api/v1", animalType);
@@ -36,6 +36,7 @@ app.use("/api/v1", order);
 app.use("/api/v1", varieties);
 app.use("/api/v1", contactUs);
 app.use("/api/v1", healthCheck);
+app.use("/api/v1", rfq);
 
 app.use("*", (req, res) => {
   res.status(404).json({
